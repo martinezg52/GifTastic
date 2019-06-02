6eea16501afdd3784b7f9e4821e39158214ab975
geo_ep <geoffrey.xwin@gmail.com>
2019-06-02 01:30:20 -0700

    Final push, working website now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,7 +40,75 @@ renderButtons();
 });
 
 $(document).on("click", ".topics", function() {
-    
-})
+
+    var topic = $(this).attr("data-name");
+
+//adding URL and API key, with limit 10 gifs
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    topic + "&api_key=FWMQ409HZVj1FLIklggd0ZDDmDQPn3xf&limit=10";
+
+//adding Ajax function to query the API and bring back image results
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .done(function(response) {
+
+        var results = response.data;
+
+//created for loop and topicDiv var and assigned it a picture class, rating and rest of classes as attribute including the 
+//data-animate class to allow it to play when clicked.
+
+        $("#imageGoHere").empty();
+
+        for (var i = 0; i < results.length; i++) {
+
+            var topicDiv = $("<div class = 'picture'>");
+
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            var topicImage = $("<img>");
+
+            topicImage.attr("src", results[i].images.fixed_height_still.url);
+
+            topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+
+            topicImage.attr("data-animate", results[i].images.fixed_height.url);
+
+            topicImage.attr("data-state", "still");
+
+            topicDiv.append(p);
+            topicDiv.append(topicImage);
+
+            $("#imageGoHere").prepend(topicDiv);
+        }
+    });
+
+});
+
+//As of right now when testing, the images do populate, but when clicked they do not play yet, classes are defined above and now need
+//to add a click event to trigger them to play
+//Also need to define a variable "state" and conditionals to have the state change from still to animate
+
+$(document).on("click", "img", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+
+    }
+});
+
+// calling again the renderButtons function so that the buttons up front for each topic show when the website first loads.
+
+renderButtons();
 
 })
